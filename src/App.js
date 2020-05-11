75891b63e9c4b1841dca46addd94ddbbfbec9a76
robertobirijnr <robertobirijnr@hotmail.com>
2020-05-11 20:53:43 +0100

    classname changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <div className="browser">
         <div className="tabs">
           <div className="tab">
