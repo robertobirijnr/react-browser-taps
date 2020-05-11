692837ff037d80f2a918391c8846518ce8c7504a
robertobirijnr <robertobirijnr@hotmail.com>
2020-05-11 20:49:03 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="browser">
+        <div className="tabs">
+          <div className="tab">
+            <a>Home</a>
+          </div>
+          <div className="tab">
+            <a>About</a>
+          </div>
+          <div className="tab">
+            <a>features</a>
+          </div>
+        </div>
+        <div className="viewport">Pages Go here</div>
+      </div>
     </div>
   );
 }
