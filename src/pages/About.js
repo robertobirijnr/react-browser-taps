1969d175ca3f7a4d3e9857eb258deb9516dc9ecc
robertobirijnr <robertobirijnr@hotmail.com>
2020-05-11 22:49:24 +0100

    adding different pages, adding router and links

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Page Goes Here!</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Features.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div>
+      <h2>Feature Page Goes Here!</h2>
+    </div>
+  );
+};
+
+export default Features;
