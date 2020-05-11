96e427c17cd8f99d1d0e410c49c6e33e2841c5b6
robertobirijnr <robertobirijnr@hotmail.com>
2020-05-11 21:32:36 +0100

    tab hightlight feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [highlightstyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  const moveHighlight = (e) => {
+    setHighlightStyle({
+      left: e.nativeEvent.layerX - 150,
+    });
+  };
+
+  const hideHighlight = (e) => {
+    setHighlightStyle({
+      opacity: 0,
+      left: e.nativeEvent.layerX - 150,
+    });
+  };
+
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
+          <div
+            className="tab"
+            onMouseOut={hideHighlight}
+            onMouseMove={moveHighlight}
+          >
+            <div className="highlight" style={highlightstyle} />
             <a>Home</a>
           </div>
           <div className="tab">
